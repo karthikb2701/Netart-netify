7b9cd0d626bc66813cc5b8d33866c8d327f7ed95
Karthhikkk <karthikb2701@gmail.com>
2021-01-03 21:43:31 +0530

    first commit

---
+++ src/components/Content.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const content= (props) => {
+    return (
+        <div className="main">
+        <div className="mid">
+            <div className="upper-row">
+               <img className="content1" src="images/1.png" alt="content" />
+               <div className="content2">
+                     <p><b>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</b></p>
+
+                     <p>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</p>
+                     <p>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.</p>
+                     <img className="content2" src="images/2.png" alt="content" />
+                 <p>Government of India has awarded the "National Energy Conservation Award 2018". Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</p>
+               </div>
+            </div>
+        </div>
+
+        <div className="second-mid">
+            <p>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION. </p>
+            <img className="content3" src="images/3.png" alt="content"></img>
+            <p>Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors</p> 
+           <hr />
+
+          <p><b> C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</b></p>
+          <p>CHEMICALS & PROCESS <vr></vr> POWER <vr></vr> WATER & WASTE WATER OILS <vr></vr> GAS PHARMA <vr></vr> SUGARS & DISTILLERIES <vr></vr> PAPER & PULP <vr></vr> MARINE & DEFENCE <vr></vr> METAL & MINING <vr></vr> FOOD & BEVERAGE <vr></vr> PETROCHEMICAL & REFINERIES <vr></vr> SOLAR <vr></vr> BUILDING <vr></vr> HVAC <vr></vr> FIRE FIGHTING <vr></vr> AGRICULTURE RESIDENTIAL</p>
+        </div>
+
+        <div className="container-fluid">
+                <div className="row">
+                    <div className="col">
+                        <p><img src="images/phone-call.png" alt="foot"></img> Toll free: 1800 200 1234</p>
+
+                    </div>
+                    <div className="col">
+                        <p><img src="images/facebook.png" alt="foot"></img> www.facebook.com/crimpumps</p>
+
+                    </div>
+                    <div className="col">
+                        <p><img src="images/world-wide-web.png" alt="foot"></img> www.crigroups.com</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    )
+ };
+ export default content; 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const header= (props) => {
+    return (
+        <div className="upper"> 
+            <img className="logo-image" src="/images/logo.png" alt="logo" />
+            
+        </div>
+    )
+ };
+ export default header; 
